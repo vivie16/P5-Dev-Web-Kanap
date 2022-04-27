2e4922affa424e2ca03e1aa3dac5c9a48bc41fe7
vivie16 <virginiedevaine@gmail.com>
2022-04-27 11:02:07 +0200

    mise a jour page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,18 +1,20 @@
 // je récupère l'id du produit a partir de l'URL
 const PRODUCTURL = new URLSearchParams(document.location.search);
 const ID = PRODUCTURL.get("_id");
-console.log(ID);
+const pageTitle = document.querySelector("title");
+//je crée les contantes pour chaque élément du produit
+const Img = document.querySelector(".item__img");
+const productTitle = document.querySelector("#title");
+const productPrice = document.querySelector("#price");
+const productDescription = document.querySelector("#description");
+const colorSelect = document.querySelector("#colors");
+// je crée la constante du bouton
+const btnPanier = document.querySelector("#addToCart")
+let kanap 
 // Je récupére le produit depuis mon API
 fetch(`http://localhost:3000/api/products/${ID}`)
   .then((res) => res.json())
   .then((kanap) => {
-    //je crée les contantes pour chaque élément du produit
-    const pageTitle = document.querySelector("title");
-    const Img = document.querySelector(".item__img");
-    const productTitle = document.querySelector("#title");
-    const productPrice = document.querySelector("#price");
-    const productDescription = document.querySelector("#description");
-    const colorSelect = document.querySelector("#colors");
     // Je place les données aux bons endroits sur la page
     pageTitle.textContent = kanap.name;
     Img.innerHTML = `<img src="${kanap.imageUrl}" alt="${kanap.altTxt}">`;
@@ -25,7 +27,7 @@ fetch(`http://localhost:3000/api/products/${ID}`)
         `<option value="${colors}">${colors}</option>` ;
       }
   })
-// je récupère les donnés de couleur et de quantité
+  // je récupère les donnés
 //couleur
 colorSelect.addEventListener ("input", (event) => {
   const colorProduct = event.target.value;
@@ -35,9 +37,15 @@ const quantitySelect = document.querySelector("#quantity");
 quantitySelect.addEventListener ("input",(event) => {
   const quantityProduct = event.target.value;
 });
-
-//je crée les conditions pour ajouter au panier
-const ajouterAuPanier = document.querySelector ("#addToCart");
-ajouterAuPanier.addEventListener("click",() => {
-  if ()
-};
+// local storage
+let storageProducts = [];
+// si le localstorage existe j'ajoute le nouveau produit
+if(localStorage.getItem ("products") !== null){
+  console.log(localStorage.getItem("products"))
+  storageProducts = JSON.parse(localStorage.getItem("products"));
+}
+//si le local storage n'existe pas
+btnPanier.addEventListener ("click",()=> {
+  storageProducts.push (kanap);
+  localStorage.setItem("products",JSON.stringify(storageProducts));
+})
